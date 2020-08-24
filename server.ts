086d159d297f91a126a5731765c7c236b3b7d454
Katarzyna Bulicka <katarzyna.bulicka@asseco.pl>
2020-08-24 13:49:37 +0200

    fix / corrected of server headers
    Signed-off-by: Katarzyna Bulicka <katarzyna.bulicka@asseco.pl>

--- server.ts
+++ server.ts
@@ -4,63 +4,17 @@ import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
 const bodyParser = require('body-parser');
 import {join} from 'path';
-var cors = require('cors');
 import { existsSync } from 'fs';
 import { AppServerModule } from './src/app/app.server.module';
 import { APP_BASE_HREF } from '@angular/common';
 const fs = require('fs');
 import * as path from 'path';
-const userFiles = './';
-// const app = express();
-
-// app.use(cors())
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-// app.use(require('body-parser').json({
-//     type: '*/*'
-// }));
-
-// app.use(function(req, res, next) {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
-// const userFiles = './';
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.use('/files', express.static(userFiles));
-// app.get('/', function(req, res) {
-//     res.json({
-//         message: 'Server Started!'
-//     });
-// });
-
-// app.post('/get-file', function(req, res, next) {
-//     const file = req.body;
-//     fs.writeFile(userFiles + file.name, file.content, (err) => {
-//         if (err) {
-//             console.error(err);
-//             res.sendStatus(500);
-//         } else {
-//             res.status(200);
-//             var filePath = path.join(__dirname) + '/' + file.name;
-//             res.sendFile(filePath);
-//         }
-//     });
-
-// });
-
-// app.listen(3000, () => console.log('Server started on port 3000'));
-
+import { Request, Response, NextFunction } from 'express';
 
 export function app(): express.Express {
     const server = express();
     const distFolder = join(process.cwd(), 'dist/zadanie/browser');
     const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
-
 
     server.engine('html', ngExpressEngine({
         bootstrap: AppServerModule,
@@ -82,29 +36,23 @@ export function app(): express.Express {
     server.get('*.*', express.static(distFolder, {
         maxAge: '1y'
     }));
-
-
     server.get('*', (req, res) => {
-        console.log('req ', req.baseUrl);
         res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
     });
-    server.post('/api/get-file', (req, res, next) => {
-        const file = req.body;
 
-        fs.writeFile(path.join(__dirname) +  '/download.xml', file.content, (err) => {
+    server.post('/api/get-file', (req: Request, res: Response, next: NextFunction) => {
+        const file = req.body;
+        fs.writeFile(path.join(__dirname) +  '/' + file.name , file.content, (err) => {
             if (err) {
                 console.error(err);
                 res.sendStatus(500);
             } else {
                 res.status(200);
-                // path.join(__dirname) +
-                const filePath = path.join(__dirname) + '/download.xml' ; // + file.name;
+                const filePath = path.join(__dirname) + '/' + file.name;
                 res.sendFile(filePath);
             }
         });
-
     });
-
     return server;
 }
 
--- src/app/core/services/file.service.ts
+++ src/app/core/services/file.service.ts
@@ -16,15 +16,15 @@ export class FileService {
   public getFileXML(fileName: string, fileContent: any): Observable<Blob> {
    return this.httpClient.post('/api/get-file', { name: fileName, content: fileContent }, {
       responseType: 'blob',
-      headers: new HttpHeaders({ 'Content-Type': 'application/xml' }).set('Accept', 'application/xml, text/plain, */*')
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }).set('Accept', 'application/xml, text/plain, */*')
     }).pipe(
       retryWhen(errors =>
         errors.pipe(
           delay(1000),
           tap(errorStatus => {
-            // if (!errorStatus.startsWith('5')) {
-            //   throw errorStatus;
-            // }
+            if (!errorStatus.startsWith('5')) {
+              throw errorStatus;
+            }
             this.loggerService.info('Retrying...');
           })
         )
